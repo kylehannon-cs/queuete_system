1dee70cc0672a0fbee8b2b01f80223ee10421672
Kyle Hannon <hannonk@oregonstate.edu>
2020-04-17 01:39:31 -0700

    First page up and running! Responsive as well.

---
+++ lib/LandingPage/LandingPage.dart
@@ -0,0 +1,67 @@
+import "package:flutter/material.dart";
+
+class LandingPage extends StatelessWidget {
+
+  List<Widget> pageChildren(double width){
+    return <Widget>[
+      Container(
+        width: width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Beep Boop. This will change", style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 40.0,
+              color: Colors.white
+            ),),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Text("Are you qeueute enough to play?",
+              style: TextStyle(
+                fontSize: 16.0, color: Colors.white
+                )
+              ),
+            ),
+            MaterialButton(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20.0))
+              ),
+              onPressed: (){},
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+                child: Text(
+                  "Available Qeueuties",
+                  style: TextStyle(color: Colors.red)
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 40.0),
+        child: Image.asset(
+          "assets/images/animal-pet-cute-baby-50577.jpg",
+          width: width
+        ),
+      )
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > 800) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: pageChildren(constraints.biggest.width/2),
+        );
+      } else {
+        return Column(
+          children: pageChildren(constraints.biggest.width),
+        );
+      }
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
+import 'package:queuete_system/LandingPage/LandingPage.dart';
+import 'package:queuete_system/Navbar/Navbar.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Queuete System',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: "IBMPlexMono"
       ),
-      home: MyHomePage(title: 'Queute System Home Page!!'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'On a scale of [1 - queuete], you are this queuete                          hint: press the plus to increase queuetness',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [
+                  Color.fromRGBO(210, 247, 136, 1.0),
+                  Color.fromRGBO(110, 163, 5, 1.0)
+            ])),
+        child: SingleChildScrollView(
+                  child: Column(
+            children: <Widget>[NavBar(), Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+              child: LandingPage(),
+            )],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
